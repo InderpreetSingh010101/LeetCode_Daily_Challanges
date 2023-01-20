d4791ba2e7ff298328d5017cfdce81c6be61a29e
InderpreetSingh010101 <79634289+InderpreetSingh010101@users.noreply.github.com>
2023-01-21 02:09:53 +0530

    Time: 13 ms (62.80%), Space: 49 MB (73.66%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -1,5 +1,9 @@
 class Solution {
     
+    
+    // In this we only doing mainly these steps
+    // creating every pair like 4  , 4,6  , 4,6,7 in form of array list
+    
     public HashSet<List<Integer>> hs = new HashSet<>() ;
     
     public List<List<Integer>> findSubsequences(int[] nums) {
@@ -11,8 +15,8 @@ class Solution {
     }
    
     public void rec(int idx , List<Integer>arr , int[] nums){
-        if(arr.size() >= 2){
-            hs.add(new ArrayList(arr));
+        if(arr.size() >= 2){  //after creating a pair we pass it and here it is chk  
+            hs.add(new ArrayList(arr)); // after chking it if size of list >2 then it is added in hashset
         }
         
         for(int i  = idx ;  i < nums.length ; i++){
